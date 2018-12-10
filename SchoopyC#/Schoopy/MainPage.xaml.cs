dceb5f73f425029d767896fe09df7c9052b5b389
PythoBanger <s.unterkoflet@gmail.com>
2018-12-10 09:41:05 +0100

    Add files via upload

---
+++ SchoopyC#/Schoopy/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Schoopy
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Window
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SchoopyC#/Schoopy/Stundenplan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schoopy
+{
+    class Stundenplan
+    {
+
+        public Stunde Montag { get; set; }
+        public Stunde Dienstag { get; set; }
+        public Stunde Mittwoch { get; set; }
+        public Stunde Donnerstag { get; set; }
+        public Stunde Freitag { get; set; }
+    }
+}
